23836e1afb6b0ac56e68ce7dac0562ef8aca3af2
Naveen Kumar G <77113035+onlynavv@users.noreply.github.com>
2021-10-21 11:10:30 +0530

    Update documentation changes

--- GITHUB - V.1/script.js
+++ GITHUB - V.1/script.js
@@ -55,19 +55,29 @@ userRepoDiv.append(userRepoRow)
 /**
 * function to  get a user repo's from github api
 *
-* getUsers is a async function, where we try to fetch the github api, where async makes the function asynchronous in the synchronous js world, without freezing the entire program
+* getUsers is a async function, where we try to fetch the github api, where async makes the function asynchronous 
+* in the synchronous js world, without freezing the entire program
 */
 
 const getUsers = async() => {
 
     /**
-        * try catch block - they are generally used to handle errors in the program, to avoid from an error causing your program, try block executes the lines of code, catch block executes the statements if an exception is thrown in the try block
+        * try catch block - they are generally used to handle errors in the program, to avoid from an error causing 
+        * your program, try block executes the lines of code, catch block executes the statements if an exception 
+        * is thrown in the try block
         *
-        * if it is a traditional error like if you have missed/misspelled a word in the url then it will throw a syntax error unexpected token in json, something wrong in the program the json can't run on it
-        * in fetch even if the response is between 400 - 500 status code which means its an error that we cant get the data back but its not going to throw an error, because the fetch can still resolve it gets a response, the only thing the fetch cannot resolve is the response when we try to run json() to that response we get the error, then  it will throw the error
-        * example: network error will trigger the catch block - TypeError - failed to fetch, resources are failed to fetched
+        * if it is a traditional error like if you have missed/misspelled a word in the url then it will throw a 
+        * syntax error unexpected token in json, something wrong in the program the json can't run on it
+        * in fetch even if the response is between 400 - 500 status code which means its an error that we cant get 
+        * the data back but its not going to throw an error, because the fetch can still resolve it gets a response, 
+        * the only thing the fetch cannot resolve is the response when we try to run json() to that response we get 
+        * the error, then  it will throw the error
+        * example: network error will trigger the catch block - TypeError - failed to fetch, resources are failed
+        * to fetched
         *
-        * in that traditional case to know what error we can simple throw an error message on our own, by checking if the response ok property is true or false, if false throw the error with status and the statusText when the json try to run on that response (unexpected token)
+        * in that traditional case to know what error we can simple throw an error message on our own, by checking if 
+        * the response ok property is true or false, if false throw the error with status and the statusText when the 
+        * json try to run on that response (unexpected token)
         *
         * throw - allows us to create own error
      */
@@ -75,7 +85,9 @@ const getUsers = async() => {
     try {
 
         /**
-            * fetch() method to get the response from the api, await will make the function to wait until the promise returns a result, await keyword can be used only inside the async function, otherwise it will throw error, it will make the async function to wait not the entire program
+            * fetch() method to get the response from the api, await will make the function to wait until the promise 
+            * returns a result, await keyword can be used only inside the async function, otherwise it will throw error, 
+            * it will make the async function to wait not the entire program
             *
             * @param {string} url - github api url
          */
@@ -117,13 +129,18 @@ const getUsers = async() => {
                 `
 
             /**
-             * map() function - goes throw the array list each and every item in that array, for each item we are appending the div's which we have created using innerHTML
+             * map() function - goes throw the array list each and every item in that array, for each item we are 
+             * appending the div's which we have created using innerHTML
              */
             
             userRepos.map((repo)=>{
 
                 /**
-                 * ternary operator ?: - here used to check if the property in the json value is present make use of that value, if not present leave/have alternate text to that field, this is because sometimes in json we might see missing property for some objects, they can lead to error in some cases while iterating to avoid that we can use this, this can also be done using "optional chaining" method ?. , only if it is present display it to the user otherwise don't
+                 * ternary operator ?: - here used to check if the property in the json value is present make use 
+                 * of that value, if not present leave/have alternate text to that field, this is because sometimes 
+                 * in json we might see missing property for some objects, they can lead to error in some cases while 
+                 * iterating to avoid that we can use this, this can also be done using "optional chaining" method ?. , 
+                 * only if it is present display it to the user otherwise don't
                  */
 
                 const description = repo.description ? repo.description : 'no description'
